8d6a3d9d28e6a5631ca580cce247551296fb99fe
hee kim <heekim@heeui-MacBookPro.local>
2026-09-04 13:20:37 +0900

    feat: input 컨포넌트 추가

--- react-common-setting/src/App.jsx
+++ react-common-setting/src/App.jsx
@@ -1,7 +1,39 @@
-import "./App.css";
+import { useState } from "react";
+import { Input } from "./components/Input";
 
-function App() {
-  return <></>;
+export default function App() {
+  const [nickname, setNickname] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  return (
+    <div>
+      <Input
+        label="닉네임"
+        placeholder="닉네임을 입력해 주세요"
+        value={nickname}
+        onChange={(e) => setNickname(e.target.value)}
+      />
+
+      <Input
+        label="비밀번호"
+        type="password"
+        placeholder="비밀번호를 입력해 주세요"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Input
+        label="비밀번호 확인"
+        type="password"
+        placeholder="비밀번호를 다시 한 번 입력해 주세요"
+        value={passwordConfirm}
+        onChange={(e) => setPasswordConfirm(e.target.value)}
+        error={
+          password !== passwordConfirm && passwordConfirm.length > 0
+            ? "비밀번호가 일치하지 않습니다."
+            : ""
+        }
+      />
+    </div>
+  );
 }
-
-export default App;

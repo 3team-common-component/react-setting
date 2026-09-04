c43014f74859bd95672081d1120468c9d58be7e3
Limug-g <dmlrbs94@gmail.com>
2026-09-04 13:23:18 +0900

    feature: Button 컴포넌트 구현

--- react-common-setting/vite.config.js
+++ react-common-setting/vite.config.js
@@ -1,11 +1,18 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+//Lim- assets 접근경로 alias를 위한 추가 코드
+import path from "path";
+import { fileURLToPath } from "url";
+//여기까지
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// https://vite.dev/config/  Lim-아래 resolve부분 추가
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") },
+  },
+});
